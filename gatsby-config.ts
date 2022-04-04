25e83fdc86da3ae642c7dbdf14b837d83f9b5481
nickheal <nick3_4@hotmail.com>
2022-04-04 19:48:48 +0100

    fix: Fix manifest

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,10 +4,10 @@ const config: GatsbyConfig = {
   pathPrefix: `/power-reader`,
   siteMetadata: {
     title: `new`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://nickheal.github.com/power-reader`
   },
   plugins: ["gatsby-plugin-jss", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
+    resolve: "gatsby-plugin-manifest",
     options: {
       "icon": "src/images/icon.png"
     }
